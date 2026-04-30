d12955a0820e4c365c9595b7439a86328800d66a
Pawan Kumar <pa.one0611@gmail.com>
2026-04-30 23:44:53 +0530

    Update unconstrained.R

--- unconstrained.R
+++ unconstrained.R
@@ -20,13 +20,13 @@ snacks_item <- sample_prop %>%
 ##what sample to take such that all the constraints are satisfies
 consumed <- rbind(breakfast_item, snacks_item, dinner_item)
 
-n=1
-r1 <- abs(rda_prop[1,5:13] - colMeans(consumed[,2:10]))
+n=5
+r1 <- abs(rda_prop[n,5:13] - colMeans(consumed[,2:10]))
 rownames(r1) <- NULL
 #r2 <- r1 / rowSums(r1)
 
 #obj_val <- data.frame()
-#sub_logi <- sample$lunch == 1
+sub_logi <- sample$lunch == 1
 obj_val <- apply(lunch[, 2:10], 1, function(row) {
   rowSums(row * log(row / r1) - row + r1) |>
     rbind()
@@ -36,9 +36,14 @@ obj_val$V2 <- as.numeric(obj_val$V2)
 names(obj_val)[2] <- "val"
 x=head(obj_val[order(obj_val$val),], 10)
 y= sample_prop$food %in% x$food_name
-a= sample[y,]  ##nutrients of consumed items in g
+a= sample[y,]  ##nutrients of low obj_val items in g
 y= sample_prop$food %in% consumed$food
-b= sample[y,]  ##nutrients of low obj. val items in grams
-rda_sub[n,]
+b= sample[y,]  ##nutrients of consumed items in grams
 ##ear_sub have all values in grams
+
+consumed[,1:6] ##in proportions
+a[1:5,1:7] ##low obj_val food
+colSums(b[,2:10])+ (a[1,2:10])  ##final nutrient intake in grams
+rda_sub[n,]  ##rda values in grams
+l values in grams
 b[1,2:10]+ colSums(a[,2:10])
