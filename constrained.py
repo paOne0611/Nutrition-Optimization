bda699a6635bb10f8b99a79118f0099ba1fecab6
Pawan Kumar <pa.one0611@gmail.com>
2026-05-01 21:46:00 +0530

    Update constrained.py

--- constrained.py
+++ constrained.py
@@ -7,24 +7,19 @@ loc=""   ##give location where saved the .RData file in your device containing t
 x= pyreadr.read_r(loc)
 x.keys()
 
+
 ###nutrients in proportions for 100g lunch given the consumed items
 data=x['sample_prop']
 
-mi=x['mij']
-ni=x['nij']
+body=5
+ear_g=x['ear_sub'].iloc[0:6,]
+tul_g=x['tul']
 rda_prop= x['rda_prop']
-#print(data.count())
-#print(data.shape)
-xij= data.iloc[:, 1:10]
+t=tul_g.iloc[body,]
+tul_prop=t[3:]/sum(t[3:])
+e= ear_g.iloc[body,]
+ear_prop= e[3:]/sum(e[3:])
 
-m = pd.to_numeric(mij.iloc[0, 4:]).to_numpy()
-m= m/m.sum()
-n = pd.to_numeric(nij.iloc[0, 4:]).to_numpy()
-n= n/n.sum()
-rda = pd.to_numeric(rda_prop.iloc[0,4:]).to_numpy()
-
-# fix lower bounds that exceed upper bounds
-n_fixed = np.minimum(n, m)
 
 breakfast_item = data[data["breakfast"]==1].sample(n=1, replace= False)
 dinner_item = data[data["dinner"]==1].sample(n=1, replace= False)
@@ -36,28 +31,30 @@ consumed = pd.concat(
     axis=0)
 
 consumed_prop = consumed.iloc[:, 1:10].mean()
-r1 = abs(rda_prop.iloc[0, 4:13] - consumed_prop)
-m1= abs(m- consumed_prop)
-n1= ab(n_fixed- consumed_prop)
-#r1 = np.abs(rda_prop.iloc[0, 4:13] - consumed.iloc[:, 1:10].sum())
 
+m = abs(pd.to_numeric(tul_prop- consumed_prop)).to_numpy()
+m= m/m.sum()
+n = abs(pd.to_numeric(ear_prop - consumed_prop)).to_numpy()
+n= n/n.sum()
+n = np.minimum(m, n)
 
 var = cp.Variable(9)
 obj = cp.Minimize(cp.sum(cp.kl_div(var, r1))) #(KL Divergence to reference r)
 cons = [
-    var <= m1, 
-    var >= n1,
+    var <= m, 
+    var >= n,
     cp.sum(var)==1
 ]
 
 prob = cp.Problem(obj, cons)
 prob.solve()
 
-x['rda_sub'].iloc[body,:]
+x['rda_sub'].iloc[body,3:]
 rda_prop.iloc[body,3]
 consumed.iloc[:,0]
 print("Status:", prob.status)
 if prob.status == 'optimal':
     print("Optimal Nutrients:\n", var.value)
-)
+
+
 
